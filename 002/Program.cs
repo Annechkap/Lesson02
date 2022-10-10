cf43032ca66a0597827bbacd04e1f1caa728eef5
Anna <annechkap@list.ru>
2022-10-10 16:19:59 +0300

    Добавлена задача 2

---
+++ 002/Program.cs
@@ -0,0 +1,7 @@
+﻿// 2. Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+int number=new Random().Next(100,1000);
+Console.WriteLine($"Случайное число {number}");
+int newnumber = (number/100) *10 + number%10;
+Console.WriteLine($"Число без второй цифры: {newnumber}");
+
